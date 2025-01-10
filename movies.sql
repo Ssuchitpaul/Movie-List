a51ed21572766a7af953e12b885bb2340c540719
Ssuchitpaul <129200211+Ssuchitpaul@users.noreply.github.com>
2025-01-10 13:01:53 +0530

    Update movies.sql
    updated the mysql of the movies project

--- movies.sql
+++ movies.sql
@@ -15,16 +15,15 @@ CREATE TABLE movies (
     box_office_total DECIMAL(15, 2)
 );
 
-SELECT * FROM MOVIES;
 
 INSERT INTO movies 
-VALUES (1, 'hulk Smash', 'Chris', 'Sci-Fi', 2010, 148, 9.0, 'https://shorturl.at/v5yw0', 850000000.00);
+VALUES (1, 'hulk Smash', 'Chris', 'Sci-Fi', 2010, 148, 9.0, 'https://shorturl.at/h32dC', 850000000.00);
 
 INSERT INTO movies 
-VALUES (2, 'The Godfather', 'Francis Ford Coppola', 'Crime', 19799, 175, 9.2, 'https://shorturl.at/QG22j', 246120974.00);
+VALUES (2, 'The Godfather', 'Francis Ford Coppola', 'Crime', 19799, 175, 9.2, 'https://shorturl.at/FJhJ2', 246120974.00);
 
 INSERT INTO movies 
-VALUES (3, 'THOR', 'Quentin Tarantino', 'Crime', 1994, 154, 8.9, 'https://shorturl.at/v0lFV', 213928762.00);
+VALUES (3, 'THOR', 'Quentin Tarantino', 'Crime', 1994, 154, 8.9, 'https://shorturl.at/ILAnU', 213928762.00);
 
 INSERT INTO movies 
 VALUES (4, 'Interstaller', 'Christopher Nolan', 'Sci-Fi', 2010, 148, 9.0, 'https://rb.gy/pk5f7f', 850000000.00);
@@ -33,8 +32,23 @@ INSERT INTO movies
 VALUES (5, 'Inception', 'Christopher Nolan', 'Sci-Fi', 2010, 148, 9.0, 'https://rb.gy/2eezz7', 850000000.00);
 
 INSERT INTO movies 
-VALUES (6, 'iron man', 'Logan', 'sci-fi', 2010, 48893, 10.0, 'https://shorturl.at/815lL', 1232.00);
+VALUES (6, 'iron man', 'Logan', 'sci-fi', 2010, 48893, 10.0, 'https://shorturl.at/xls3o', 1232.00);
 
 INSERT INTO movies 
 VALUES (7, 'Joker', 'Joaquin', 'drama', 2019, 185, 8.0, 'https://rb.gy/mhxlzx', 123424.00);
+
+
+create table users(id int auto_increment primary key,username varchar(100), email varchar(100), password varchar(100),role varchar(100));
+
+INSERT INTO users (username, email, password, role) 
+VALUES 
+('Alice', 'alice@example.com', 'password123', 'admin'),
+('Bob', 'bob@example.com', 'password456', 'user'),
+('Charlie', 'charlie@example.com', 'password789', 'user'),
+('Dave', 'dave@example.com', 'password101112', 'admin'),
+('Eve', 'eve@example.com', 'password131415', 'user');
+
+create table fav (userid int,movieid int,movie varchar(100));
+
+create table req(userid int,movieid int,movie varchar(100),status varchar(100));
 
